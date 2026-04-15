b036c572bda1b711a4454a8013665dee6105221e
divijg19 <divijganjoo2003@gmail.com>
2026-04-15 23:57:00 +0530

    Monorepo DX & Environment Management Attunement

--- apps/engine/main.py
+++ apps/engine/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Any, cast
 from api.routes import router as api_router
 from api.stream import router as stream_router
 
@@ -7,7 +8,7 @@ from api.stream import router as stream_router
 app = FastAPI(title="Agora Engine", version="0.1.4")
 
 app.add_middleware(
-    CORSMiddleware,
+    cast(Any, CORSMiddleware),
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
